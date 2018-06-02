b5a5daee5c84a225c27ad5ecf53435b5d5a6d499
Edward Kruljac <ewkruljac@gmail.com>
2018-06-02 12:15:35 -0600

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :blogs
 
   get 'about-me',   to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get 'contact',    to: 'pages#contact'
   
   root to: 'pages#home'
 end
