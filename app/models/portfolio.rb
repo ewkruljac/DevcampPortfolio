1b44fccf9b20d88c7bd0a5a72d61e96bf8b90c6a
Edward Kruljac <ewkruljac@gmail.com>
2018-06-14 14:42:52 -0600

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,14 +5,17 @@ class Portfolio < ApplicationRecord
 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
-
+  
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+  
   # Two ways to set scopes
   def self.angular
     where(subtitle: "Angular")
   end
 
   def self.by_position
-    order("position ASC").limit(9)
+    order("position ASC")
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
