402756ef1b186613f9cc5be0b8c13952231fefee
Edward Kruljac <ewkruljac@gmail.com>
2018-06-20 20:40:49 -0600

    Updated source tracking feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,12 @@ module ApplicationHelper
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you 
-                  are on the #{layout_name} layout."
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, 
+        please feel free to #{ link_to 'contact me', contact_path} 
+        if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
@@ -68,11 +69,5 @@ module ApplicationHelper
   def alert_generator(msg)
     js add_gritter(msg, title: "Edward Kruljac's Portfolio")
   end
-
-
-
-
-
-
 
 end
